f3000faa99d991a91c402ba98d126cd13758163f
Chris DeHaan <christopherwdehaan@gmail.com>
2024-03-15 16:20:14 -0400

    initial look at Kotlin variables

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,13 @@
 fun main () {
-    println("Hello, World!")
+    var userName: String = "John"
+    userName = "Chris"
+
+    var age: Int = 22
+    age = 31
+
+    // Kotlin infers what the type is based on the value you assign it
+    var kotlinInfers = "test"
+    kotlinInfers = 3
+
+    println("Hello $userName! Your age is $age.")
 }
